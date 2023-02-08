b4860494fbb3c1a4b21a8eb2c8cb87d4c1a4f5c4
Charvel Johnson <cjohnson42@tntech.edu>
2023-02-08 11:42:18 -0600

    Created db.Service.ts skeleton

---
+++ src/app/indexedDB/utilityMeterScanProfile-db.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { NgxIndexedDBService } from 'ngx-indexed-db';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { IdbAccount, IdbCustomEmissionsItem } from '../models/idb';
+import { AccountdbService } from './account-db.service';
+
+export class UtilityMeterScanProfileService {
+    
+}

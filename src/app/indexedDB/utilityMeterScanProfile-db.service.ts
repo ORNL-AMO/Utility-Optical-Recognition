3240b4f3c3a0f06c6320f9c4ae563a6b8547807c
Charvel Johnson <cjohnson42@tntech.edu>
2023-02-13 11:13:51 -0600

    Added Injectale

--- src/app/indexedDB/utilityMeterScanProfile-db.service.ts
+++ src/app/indexedDB/utilityMeterScanProfile-db.service.ts
@@ -4,6 +4,9 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { IdbAccount, IdbCustomEmissionsItem } from '../models/idb';
 import { AccountdbService } from './account-db.service';
 
+@Injectable({
+    providedIn: 'root'
+})
 export class UtilityMeterScanProfileService {
     
 }

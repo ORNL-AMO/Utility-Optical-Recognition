cfc833c298327ef53d9bfd951438b8b1fa134f1b
Charvel Johnson <cjohnson42@tntech.edu>
2023-02-14 11:59:46 -0600

    Updated Variable names

--- src/app/indexedDB/utilityMeterScanProfile-db.service.ts
+++ src/app/indexedDB/utilityMeterScanProfile-db.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { NgxIndexedDBService } from 'ngx-indexed-db';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { IdbAccount, IdbCustomEmissionsItem } from '../models/idb';
+import { IdbAccount,  utilityMeterScanProfile} from '../models/idb';
 import { AccountdbService } from './account-db.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UtilityMeterScanProfileService {
-    
+
+    utilityMeterScanProfileItems: BehaviorSubject<Array<utilityMeterScanProfile>>; //Getting it from idb.ts
+    constructor(private dbService: NgxIndexedDBService, private accountDbService: AccountdbService) { 
+        this.utilityMeterScanProfileItems = new BehaviorSubject<Array<utilityMeterScanProfile>>([]);
+    }
+
+    getAll(): Observable<Array<utilityMeterScanProfile>> {
+        return this.dbService.getAll('');
+    }
 }

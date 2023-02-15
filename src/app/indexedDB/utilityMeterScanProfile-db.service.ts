18a176fe7cd14df22bbf57ff213b29cb33163237
Charvel Johnson <cjohnson42@tntech.edu>
2023-02-15 11:15:08 -0600

    New Profile

--- src/app/indexedDB/utilityMeterScanProfile-db.service.ts
+++ src/app/indexedDB/utilityMeterScanProfile-db.service.ts
@@ -18,5 +18,39 @@ export class UtilityMeterScanProfileService {
         return this.dbService.getAll('utilityMeterScanProfile');
     }
 
+    addWithObservable(utilityMeterScanProfileItem: utilityMeterScanProfile): utilityMeterScanProfile { //CRUD > Create
+        // return this.dbService.add('utilityMeterScanProfile', utilityMeterScanProfileItem);
+        //Add more here.
+        return {
+            id?: selectAccount.guid,
+            guid: Math.random().toString(36).substr(2, 9),
+            accountId: "",
+            source: new MeterSource,
+            attribute: AttributeTypes,
+            x1: number,
+            y1: number,
+            x2: number,
+            y2: number,
+        }
+    }
     
+    deleteWithObservable(guid: string, ): Observable<any> { //CRUD > Delete
+        return this.dbService.delete('utilityMeterScanProfile', guid);
+    }
+    
+    updateWithObservable(values: utilityMeterScanProfile): Observable<utilityMeterScanProfile> { //CRUD > Update
+        return this.dbService.update('utilityMeterScanProfile', values);
+    }
+
+    // getNewAccountEmissionsItem(selectedAccount: IdbAccount): IdbCustomEmissionsItem {
+    //     // let selectedAccount: IdbAccount = this.accountDbService.selectedAccount.getValue();
+    //     return {
+    //         accountId: selectedAccount.guid,
+    //         id: Math.random().toString(36).substr(2, 9),
+    //         date: new Date(),
+    //         subregion: 'New Custom Subregion',
+    //         locationEmissionRates: [],
+    //         residualEmissionRates: []
+    //     }
+    // }
 }

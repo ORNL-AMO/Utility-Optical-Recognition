eea01c9877986feaba2c1628b71ec0cc7448cfde
will-t <w98434545@gmail.com>
2023-03-29 15:36:57 -0500

    #52 Added basic parsing ability of know presets

--- src/app/utility-optical-recognition/utility-optical-recognition.component.ts
+++ src/app/utility-optical-recognition/utility-optical-recognition.component.ts
@@ -1,12 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { PDFDocumentProxy } from 'ng2-pdf-viewer';
 import html2canvas from 'html2canvas';
-import { ImageCroppedEvent } from 'ngx-image-cropper';
+import { Dimensions, ImageCroppedEvent, ImageTransform, ImageCropperComponent, CropperPosition } from 'ngx-image-cropper'
 import { createWorker } from 'tesseract.js'
 import { IdbUtilityMeter, IdbUtilityMeterData, utilityMeterScanProfile } from '../models/idb';
 import { SourceOptions } from 'src/app/facility/utility-data/energy-consumption/energy-source/edit-meter-form/editMeterOptions';
 import * as _ from 'lodash';
 import { UtilityMeterScanProfileService } from '../indexedDB/utilityMeterScanProfile-db.service';
+import { exit } from 'process';
+import { Interface } from 'readline';
+import { element } from 'protractor';
+import { color } from 'html2canvas/dist/types/css/types/color';
+import { AnyCnameRecord } from 'dns';
 
 @Component({
   selector: 'app-utility-optical-recognition',
@@ -16,24 +21,30 @@ import { UtilityMeterScanProfileService } from '../indexedDB/utilityMeterScanPro
 
 export class UtilityOpticalRecognitionComponent implements OnInit {
   //#region Variables
+  @ViewChild('pdfViewer') pdfViewer;
   @Input() editMeter: IdbUtilityMeter;
   @Input() editMeterData: IdbUtilityMeterData;
+  public page: number = 1;
   public undefinedMeterData;
-  
+  public counterVar: number = 0;
+  public imageChangedEvent: any = '';
+  public showFileUploadDiv1:boolean = false;
   public showScanProfileSelectorDiv: boolean = true;        //aka preset profiles
   public showUtilitySelectorDiv: boolean = false;
   public showFileUploadDiv: boolean = false;
   public showPdfModalDiv: boolean = false;
   public showCropButtons: boolean = false;
   public showPdfDiv: boolean = false;
-
+  public showPdfModalDiv1: boolean = false;
   public isPdfUploaded: boolean = false;
   public isPdf2Image: boolean = false;
+  public isPdf2Image1: boolean = false;
   public isOcrResult: boolean = false;
   public pdfSrc: any = '';
   public totalPages: number = 0;
   public currentpage: number = 1;
   public cropingImage: any = '';
+  public grabbingImage: any = '';
   public ocrResult: any = '';
   public newScanProfile: any;
   public presetName: string = '';
@@ -45,7 +56,23 @@ export class UtilityOpticalRecognitionComponent implements OnInit {
   public last_attritbute = '';
   public JSON_object = {};
   public uniqueProfiles: utilityMeterScanProfile[];
-  public inputValue123: any = (<HTMLInputElement>document.getElementById("inputDiv"))
+  public inputValue123: any = (<HTMLInputElement>document.getElementById("inputDiv"));
+
+  //mabe
+  public pdf: PDFDocumentProxy;
+  public GetProfile =
+  {
+    guid: "",
+    accountId: "",
+    name123: "",
+    source123: "",
+    attribute123: "",
+    coordinatesx1: 0,
+    coordinatesy1: 0,
+    coordinatesx2: 0,
+    coordinatesy2: 0,
+    pgNum: 1,
+  };
   public interface = 
   {
     guid: "",
@@ -58,19 +85,24 @@ export class UtilityOpticalRecognitionComponent implements OnInit {
     coordinatesx2: 0,
     coordinatesy2: 0,
     pgNum: 1,
-  }
-  public colorIndex: number = null;
-
-  // public sourceOptions: Array<string> = SourceOptions;      // provides the types of utilities
-
+  };
+  public colorIndex: number = 0;
+  cropperPosition: CropperPosition = {
+    x1: 0,
+    y1: 0,
+    x2: 0,
+    y2: 0
+  };
+ 
     //"Getter method", Angular will call the getter method whenever it needs to update the value of the `src` attribute.
     get strdPdf2Img(): string {
       return this.rtrvPdf2ImgFrmStrg() || '';
     }
-  
+    
     get strdCrppdImg(): string {
       return this.rtrvCrppdImgFrmStrg() || '';
-    }  
+    }
+
 //#endregion
 
   constructor(
@@ -115,6 +147,7 @@ export class UtilityOpticalRecognitionComponent implements OnInit {
         let reader = new FileReader();
         reader.onload = (e: any) => {
           this.pdfSrc = e.target.result;
+          
         };
         this.isPdfUploaded = true;
         this.showPdfDiv = true;
@@ -127,7 +160,13 @@ export class UtilityOpticalRecognitionComponent implements OnInit {
   }
 
   public afterLoadComplete(pdf: PDFDocumentProxy) {
-    this.totalPages = pdf.numPages;
+   this.totalPages = pdf.numPages;
+   if(this.GetProfile.pgNum != this.currentpage){
+   pdf.getPage(1).then((currentpage) => {
+    this.currentpage = this.GetProfile.pgNum;
+
+  });
+}
   }
 
   public previous() {
@@ -154,23 +193,94 @@ export class UtilityOpticalRecognitionComponent implements OnInit {
   //#endregion
 
   //#region Html2Canvas
-  public pdfToCanvas(event:any, index:number) {
+  public pdfToCanvas(event:any, index:number| null ) {
+    
+     if(this.GetProfile.pgNum != this.currentpage && index == null){
+      this.currentpage = this.GetProfile.pgNum;
+      this.pdfSrc = this.pdfSrc;
+    }
+    if(index != null){
     this.undefinedMeterData[index][1] = "red"
-    
-    // set attribute for scan profile
     this.interface.attribute123 = event.target.id;
-    
-    html2canvas(document.querySelector(".pdf-container") as HTMLElement).then((canvas: any) => {
+  }
+
+     html2canvas(document.querySelector(".pdf-container") as HTMLElement).then((canvas: any) => {
       this.getCanvasToStorage(canvas)
     })
-    
-     this.isPdfUploaded = false;
-    this.isPdf2Image = true;
-    
-    this.last_attritbute = event.target.id;
+    if(index == null){
+      this.cropperPosition.x1 = this.GetProfile.coordinatesx1;
+      this.cropperPosition.y1 = this.GetProfile.coordinatesy1;
+      this.cropperPosition.x2 = this.GetProfile.coordinatesx2;
+      this.cropperPosition.y2 = this.GetProfile.coordinatesy2;
 
-    this.updateAttributeColor(index);
+    }
+    this.isPdfUploaded = false;
+    this.isPdf2Image = true;
+    this.last_attritbute = event.target.id;
+    if(index != null){
+      this.updateAttributeColor(index);
+    }
   }
+  croppedImage: any = null;
+  aspectRatio = 1;
+  resizeToWidth = 0;
+  resizeToHeight = 0;
+  cropperMinWidth = 0;
+  cropperMinHeight = 0;
+  onlyScaleDown = false;
+  imageQuality = 100;
+  autoCrop = true;
+  backgroundColor = '';
+  disabled = false;
+  canvasRotation = 0;
+  transform = {};
+  allowMoveImage = false;
+  hidden = false;
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+  }
+
+  imageLoaded(): void {
+    // Do something when the image is loaded
+  }
+
+  cropperReady(dimensions: Dimensions): void {
+    // Do something when the cropper is ready
+    // For example, set the initial cropper position
+    const { x1, y1, x2, y2 } = this.calculateInitialPosition(dimensions);
+    this.cropperPosition = { x1, y1, x2, y2 };
+  }
+
+  calculateInitialPosition(dimensions: Dimensions): CropperPosition {
+    const x1 = this.GetProfile.coordinatesx1;// calculate x1 position based on passed x1 and dimensions
+    const y1 = this.GetProfile.coordinatesy1;// calculate y1 position based on passed y1 and dimensions
+    const x2 = this.GetProfile.coordinatesx2;// calculate x2 position based on passed x2 and dimensions
+    const y2 = this.GetProfile.coordinatesy2;// calculate y2 position based on passed y2 and dimensions
+    return { x1, y1, x2, y2 };
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   private getCanvasToStorage(canvas:any){
     let ctx = canvas.getContext('2d');
@@ -181,11 +291,12 @@ export class UtilityOpticalRecognitionComponent implements OnInit {
   //#endregion
 
   //#region Image Cropper
-  public cropImage(event: ImageCroppedEvent){
+  public cropImage(event: ImageCroppedEvent ){
     this.cropingImage = event.base64;
     sessionStorage.setItem("CrppdImg", this.cropingImage);
 
     // set coordinates for scan profile
+
     this.interface.coordinatesx1 = event.cropperPosition.x1;
     this.interface.coordinatesy1 = event.cropperPosition.y1;
     this.interface.coordinatesx2 = event.cropperPosition.x2;
@@ -198,7 +309,7 @@ export class UtilityOpticalRecognitionComponent implements OnInit {
   private rtrvPdf2ImgFrmStrg(){
     return window.sessionStorage.getItem("pdf2Img");
   }
-
+  
   private rtrvCrppdImgFrmStrg(){
     return window.sessionStorage.getItem("CrppdImg");
   }
@@ -284,6 +395,7 @@ export class UtilityOpticalRecognitionComponent implements OnInit {
     this.interface.coordinatesy2 = 0;
     this.interface.attribute123 = "";
     this.isPdfUploaded = true;
+    this.startProcessing(null);
     return;
   }
 
@@ -291,15 +403,24 @@ export class UtilityOpticalRecognitionComponent implements OnInit {
     this.onGetUniqueProfiles();
     const worker1 = createWorker();
     this.showFileUploadDiv = false;
+
     this.showScanProfileSelectorDiv = true;
     this.showUtilitySelectorDiv = false;
     this.showPdfModalDiv = false;
     this.showCropButtons = false;
     await (await worker1).terminate();
+  }
 
-    for(var index in this.undefinedMeterData){
-      this.undefinedMeterData[index][1] = "black";
-    }
+  async endProfile1(){
+    this.onGetUniqueProfiles();
+    const worker1 = createWorker();
+    this.showFileUploadDiv1 = false;
+    this.showScanProfileSelectorDiv = true;
+    this.showUtilitySelectorDiv = false;
+    this.showPdfModalDiv1 = false;
+    this.showCropButtons = false;
+
+    await (await worker1).terminate();
   }
 
   
@@ -324,6 +445,10 @@ export class UtilityOpticalRecognitionComponent implements OnInit {
     return this.JSON_object;
   }
 
+//   fileChangeEvent(event: any): void {
+//     this.imageChangedEvent = event;
+// }
+
   onGetUniqueProfiles() {
     this.scanProfileDbService.getAll().subscribe(profiles => {
       // Filter out profiles with duplicate presetName
@@ -332,5 +457,54 @@ export class UtilityOpticalRecognitionComponent implements OnInit {
       );
     });
   }
-
+  async startProcessing(event: any | null){
+    
+    if(event != null){
+      this.GetProfile.name123 = event.target.value;
+    }
+    await this.scanProfileDbService.getByPresetName(this.GetProfile.name123).subscribe(profiles => {
+      let tempArray = profiles;
+      this.GetProfile.accountId = tempArray[this.counterVar].accountId;
+      this.GetProfile.source123 = tempArray[this.counterVar].source;
+      this.GetProfile.coordinatesx1 = tempArray[this.counterVar].x1;
+      this.GetProfile.coordinatesy1 = tempArray[this.counterVar].y1;
+      this.GetProfile.coordinatesx2 = tempArray[this.counterVar].x2;
+      this.GetProfile.coordinatesy2 = tempArray[this.counterVar].y2;
+      this.GetProfile.pgNum = tempArray[this.counterVar].pgNum;
+      this.GetProfile.attribute123 = tempArray[this.counterVar].attribute;
+      if(event != null){
+      this.showFileUploadDiv1 = true;
+      this.showScanProfileSelectorDiv = false;
+      
+      }
+      this.counterVar++;
+    });
+    return;
 }
+  async doOCR2(){
+    // show/hide divs
+    this.isPdf2Image = false;
+    this.isOcrResult = true;
+    const worker = createWorker();
+    this.undefinedMeterData[this.colorIndex][1] = "lightgray"
+    await (await worker).load();
+    await (await worker).loadLanguage('eng');
+    await (await worker).initialize('eng');
+    const {data: { text } } = await (await worker).recognize(this.cropingImage);
+    sessionStorage.setItem("CrppdImg", this.cropingImage); 
+    this.ocrResult = text;
+    this.ocrResult = this.ocrResult.replace(/[$\n]/g, '') //splice out $ and new lines
+    this.add_to_json(this.last_attritbute, this.ocrResult);
+    this.set_json();
+    await (await worker).terminate();
+    this.isPdfUploaded = true;
+    this.startProcessing(null);
+    this.currentpage = 1;
+    return;
+  }
+ 
+
+
+
+//edit bill component
+  }
